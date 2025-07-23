6e501a89c4f307ed37742b75608270e234090dff
Unnyan Sharma <sharmaunnayan@gmail.com>
2025-07-23 14:56:16 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
